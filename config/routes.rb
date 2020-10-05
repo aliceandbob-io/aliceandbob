634eddf57b6ae61bac2ef486370302383d585370
theolazian <theo.lazian@gmail.com>
2020-10-05 22:00:38 +0200

    Configure Devise path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#content"
 end
