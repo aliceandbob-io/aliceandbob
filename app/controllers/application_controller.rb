878cef4ab02097a2cfd8a478825696e600e57a82
theolazian <theo.lazian@gmail.com>
2020-12-09 21:24:04 +0100

    Set social meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def default_url_options
+    if Rails.env.production?
+      { host: 'https://aliceandbob.io'}
+    else
+      { host: 'http://localhost:3000/' }
+    end
+  end
 end
